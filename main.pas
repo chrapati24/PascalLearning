5262d9dae33e3298024df066bb4fa0a5a6df3806
Chrapati <35665560+chrapati24@users.noreply.github.com>
2022-08-30 16:49:57 +0200

    Update main.pas

--- main.pas
+++ main.pas
@@ -2,10 +2,17 @@ program hello;
 
 var
 test : integer = 1;
+valueOne, valueTwo, result : integer;
 
 begin
     writeLn('hello world = ', test);
-    readLn;
+    writeLn('Please write a first value : ');
+    readLn(valueOne);
+    writeLn('Please write a second value : ');
+    readLn(valueTwo);
+    result := (valueOne + valueTwo);
+    writeLn(valueOne, ' + ', valueTwo, ' = ', result);
+    readLn();
 end.
 
 
